f8f215fc8a23684bc79f4fbdf45cd90efc688cad
Iancecil Waweru Njoroge <60706462+iancecil@users.noreply.github.com>
2025-04-10 13:25:32 +0300

    extends Animal abstract class

--- Animal.java
+++ Animal.java
@@ -25,3 +25,19 @@ public abstract class Animal {
         return name;
     }
 }
+
+
+/**
+ * Concrete class extending Animal abstract class
+ * Must implement all abstract methods from Animal
+ */
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+    
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " says: Woof woof!");
+    }
+}
