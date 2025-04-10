564e88c4e704444a8571089c2c62d3471bb9a416
Iancecil Waweru Njoroge <60706462+iancecil@users.noreply.github.com>
2025-04-10 13:28:30 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -26,18 +26,3 @@ public abstract class Animal {
     }
 }
 
-
-/**
- * Concrete class extending Animal abstract class
- * Must implement all abstract methods from Animal
- */
-public class Dog extends Animal {
-    public Dog(String name) {
-        super(name);
-    }
-    
-    @Override
-    public void makeSound() {
-        System.out.println(getName() + " says: Woof woof!");
-    }
-}
