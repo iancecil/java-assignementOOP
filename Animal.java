81ff42ad3cb74ec55ea6aec023fe19c424b98901
Iancecil Waweru Njoroge <60706462+iancecil@users.noreply.github.com>
2025-04-10 13:23:42 +0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,27 @@
+/**
+ * Abstract Class: Animal
+ * - An abstract class cannot be instantiated directly
+ * - It can contain both abstract and concrete methods
+ * - Subclasses must implement all abstract methods
+ */
+public abstract class Animal {
+    private String name;
+    
+    // Constructor (abstract classes can have constructors)
+    public Animal(String name) {
+        this.name = name;
+    }
+    
+    // Abstract method (no implementation)
+    public abstract void makeSound();
+    
+    // Concrete method with implementation
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+    
+    // Another concrete method
+    public String getName() {
+        return name;
+    }
+}
